4ed06276d734123afdbce80214fecaabe529b48c
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2025-01-07 16:13:28 -0500

    added shit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,9 +5,10 @@ export function meta({}: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
-  ];
+  ]; 
 }
 
 export default function Home() {
+  <h2>WTF</h2>
   return <Welcome />;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,18 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      colors: {
+        'background': '#fdf4e3',
+        'purple': '#936af6',
+        'pink': '#dd5e7f',
+        'orange': '#fb8f02',
+        'black': '#1e1e1e',
+        'red': '#6f1d1b',
+        'yale': '#0d3b66',
+        'green': '#5da271',
+        'space': '#283044',
+        'grey': '#8f91a2',
+      },
       fontFamily: {
         sans: [
           '"Inter"',
